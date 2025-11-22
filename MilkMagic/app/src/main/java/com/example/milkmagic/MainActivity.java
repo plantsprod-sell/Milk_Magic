b6c997323be70df54efc92d054a540f97ffb564a
plantsprod-sell <plantsprod@gmail.com>
2025-11-22 12:33:34 +0530

    Onboarding project

---
+++ MilkMagic/app/src/main/java/com/example/milkmagic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.milkmagic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);   // links to res/layout/activity_main.xml
+    }
+}
