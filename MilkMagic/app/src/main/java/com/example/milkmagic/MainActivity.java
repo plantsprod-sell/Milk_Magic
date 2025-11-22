a843fc69f9d53f838167ba046289cb5b58ec7182
plantsprod-sell <plantsprod@gmail.com>
2025-11-22 14:34:43 +0530

    Total main page

--- MilkMagic/app/src/main/java/com/example/milkmagic/MainActivity.java
+++ MilkMagic/app/src/main/java/com/example/milkmagic/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.milkmagic;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   // links to res/layout/activity_main.xml
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Handle Bottom Navigation Clicks
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
+                    // Current Screen - do nothing
+                    return true;
+                } else if (id == R.id.nav_map) {
+                    // Logic to open Map/Vendor Activity
+                    Toast.makeText(MainActivity.this, "Opening Vendor Map...", Toast.LENGTH_SHORT).show();
+                    // Intent intent = new Intent(MainActivity.this, VendorActivity.class);
+                    // startActivity(intent);
+                    return true;
+                } else if (id == R.id.nav_profile) {
+                    // Logic to open Profile Activity
+                    Toast.makeText(MainActivity.this, "Opening Profile...", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
